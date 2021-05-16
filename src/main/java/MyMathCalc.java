69487f09205050a4097ac3315ea4a97868fbfc6d
Donatas Auzelis <donatas963@gmail.com>
2021-05-16 09:33:08 +0200

    changed divide return value to double

--- src/main/java/MyMathCalc.java
+++ src/main/java/MyMathCalc.java
@@ -1,14 +1,14 @@
 public class MyMathCalc {
 
     public int add(int a, int b) {
-
+        return a + b;
     }
 
     public int multiply(int a, int b) {
-
+        return a * b;
     }
 
-    public int divide(int a, int b) {
-
+    public double divide(double a, double b) {
+        return a / b;
     }
 }
